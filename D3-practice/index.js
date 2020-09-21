c26adf169fc4bb11793fbdb63aa4182673967127
Jonathan Tse <Jonathan@Jonathans-MBP.broadband>
2020-09-21 20:47:18 +0100

    place data in JSON form in an external file and then use enter method to render data into browser

--- D3-practice/index.js
+++ D3-practice/index.js
@@ -34,26 +34,53 @@
 
 
 
-const data = [
-    {width: 200, height: 100, fill: 'purple'},
-    {width: 100, height: 60, fill: 'pink'},
-    {width: 50, height: 30, fill: 'red'}    
-];
 
+
+
+// const data = [
+//     {width: 200, height: 100, fill: 'purple'},
+//     {width: 100, height: 60, fill: 'pink'},
+//     {width: 50, height: 30, fill: 'red'}    
+// ];
+
+// const svg = d3.select('svg');
+
+// //Joins aka attach data to all rects
+// const rects = svg.selectAll('rect')
+//     .data(data);
+
+// //Add attrs to rects already in the DOM
+// rects.attr('width', (d, i, n) => d.width)
+// .attr('height', d => d.height)
+// .attr('fill', d => d.fill);
+
+// //Appends the enter selection i.e. additional rects not yet created, to DOM
+// rects.enter()
+//     .append('rect')
+//     .attr('width', (d, i, n) => d.width)
+//     .attr('height', d => d.height)
+//     .attr('fill', d => d.fill);
+
+
+
+//select svg container first
 const svg = d3.select('svg');
 
-//Joins aka attach data to all rects
-const rects = svg.selectAll('rect')
+d3.json('planets.json').then(data => {
+    const circs = svg.selectAll('circle')
     .data(data);
 
-//Add attrs to rects already in the DOM
-rects.attr('width', (d, i, n) => d.width)
-.attr('height', d => d.height)
-.attr('fill', d => d.fill);
-
-//Appends the enter selection i.e. additional rects not yet created, to DOM
-rects.enter()
-    .append('rect')
-    .attr('width', (d, i, n) => d.width)
-    .attr('height', d => d.height)
+    //add attrs to circs already in DOM
+    circs.attr('cy', 200)
+    .attr('cx', d => d.distance)
+    .attr('r', d => d.radius)
     .attr('fill', d => d.fill);
+
+    //append the enter selection to the DOM
+    circs.enter()
+        .append('circle')
+        .attr('cy', 200)
+        .attr('cx', d => d.distance)
+        .attr('r', d => d.radius)
+        .attr('fill', d => d.fill);
+})
